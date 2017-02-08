4df6256df2d07089a9572905434893f52bad14f8
Anže Pečar <anze@pecar.me>
2017-02-08 23:33:43 +0100

    Add setup.py and config files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pdf2text',
+    packages=['pdf2text'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'pdfminer.six',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
