276807d05f8eb226655d0cd50819cee2e3c30e1f
Anže Pečar <anze@pecar.me>
2017-02-12 21:05:24 +0000

    Refactor PDF parser

--- pdf2text/parser.py
+++ pdf2text/parser.py
@@ -1,22 +1,14 @@
 """PDF parser"""
 
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from StringIO import StringIO
+import pdfminer.high_level
+from tempfile import TemporaryFile
 
 
 def parse(f):
-    parser = PDFParser(f)
-    document = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    out = StringIO()
-    device = TextConverter(rsrcmgr, out)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(document):
-        interpreter.process_page(page)
-    return out.getvalue()[1:]
+    """Parse the input file f and return plain text"""
+    with TemporaryFile() as out:
+        laparams = pdfminer.layout.LAParams()
+        pdfminer.high_level.extract_text_to_fp(f, out, laparams=laparams)
+        out.seek(0)
+        lines = out.read()
+        return lines
