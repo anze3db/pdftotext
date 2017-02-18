e065c7a47ce2cbb5f2bec7f4ef86eaf0a1f21040
Anže Pečar <anze@pecar.me>
2017-02-18 22:21:26 +0000

    Add support for pages to upload()

--- pdf2text/main.py
+++ pdf2text/main.py
@@ -25,12 +25,16 @@ def index():
 def upload():
     """Serve the upload page"""
     pdf = request.files.get('pdf', None)
+    pages = request.form.get('pages', '')
     if pdf is None or not pdf.filename:
         flash('No file uploaded! Please pick a file to upload.')
         return redirect(url_for('.index'))
     try:
-        parsed = parse(pdf)
+        parsed = parse(pdf, pages)
     except PDFSyntaxError:
         flash("Uploaded file is not a valid PDF")
+        return redirect(url_for('.index'))
+    except ValueError:
+        flash('Was not able to parse page numbers')
         return redirect(url_for('.index'))
     return render_template('index.html', parsed_text=unicode(parsed, "utf8"))
